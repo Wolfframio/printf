b97c0bfce72c0713fed976909ab51fc2b4216174
Wolfframio <116112114+Wolfframio@users.noreply.github.com>
2023-06-04 23:49:19 +0200

    Add files via upload

---
+++ printfwork/ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imontero <imontero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 17:08:38 by imontero          #+#    #+#             */
+/*   Updated: 2023/06/04 17:08:38 by imontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+char	*ft_strchr(const char *s, int c);
+int		ft_printnbr(int n);
+int		ft_printstr(char *s);
+
+
+#endif
